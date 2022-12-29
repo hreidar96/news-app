d0f0092b5cb83cb8786c9b94d072d36e37367a8f
Hreiðar Ingi <hreidar220@gmail.com>
2022-12-29 23:50:31 +0000

    delete static json

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { categories } from "../constants";
 import fetchNews from "../lib/fetchNews";
 import NewsList from "./NewsList";
-import response from "../response.json";
 
 async function Homepage() {
   // fetch the news data
